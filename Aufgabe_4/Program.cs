7f416f242dedd85095b04f39b93248f3fbd44119
Dirk Mueller <dirkmuller8@outlook.de>
2021-03-14 15:48:33 +0100

    Extracted logic to separate class

--- Aufgabe_4/Program.cs
+++ Aufgabe_4/Program.cs
@@ -10,19 +10,15 @@ namespace Aufgabe_4
     // 1. Convert all characters of the given string to lower case
     // 2. Iterate through the string and check if each character is a vowel
     // 3. If so, increment the counter
-
-    class Program
+    class DrawNumbers
     {
-        static void Main(string[] args)
+        int candidateNr;
+
+        List<int> lottoZahlen = new List<int>(6);
+        Random random = new Random();
+
+        public void MakeNumbers()
         {
-            Console.WriteLine("********************************************************************************");
-            Console.WriteLine("* This program performs a drawing of the lottery 6 out of 49                   *");
-            Console.WriteLine("********************************************************************************");
-
-            int candidateNr;
-
-            List<int> lottoZahlen = new List<int>(6);
-            Random random = new Random();
             while (lottoZahlen.Count < 6)
             {
                 candidateNr = random.Next(1, 50);
@@ -35,6 +31,19 @@ namespace Aufgabe_4
             {
                 Console.WriteLine(item);
             }
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* This program performs a drawing of the lottery 6 out of 49                   *");
+            Console.WriteLine("********************************************************************************");
+
+            DrawNumbers draw = new DrawNumbers();
+            draw.MakeNumbers();
         }
     }
 }
